0da05d7a9232194ec7b08a4d9699358629dbd365
Storm750 <Storm750@users.noreply.github.com>
2019-04-01 20:45:12 -0400

    Fixed compiling issue by assigning function parameters to intake addresses. Variables in function have their own addresses

--- src/pointers.cpp
+++ src/pointers.cpp
@@ -2,11 +2,18 @@
 
 using namespace std;
 
-void myswap(int * ptr1, int* ptr2)
+void myswap(int* ptr1, int* ptr2)
 {
+	
+  cout << "ptr1 address: " << &ptr1 << endl;
+
   auto temp = ptr1;
   ptr1 = ptr2;
   ptr2 = temp;
+  
+  cout << "address of temp: " << &temp << endl << endl;
+  cout << "address of ptr1: " << &ptr1 << endl << endl;
+  cout << "address of ptr2: " << &ptr2 << endl << endl;
 }
 
 
@@ -17,12 +24,16 @@ int main()
   cout<<"What am I doing wrong ☹ \n\n";
 
   cout<<"a = "<<a<<", b = "<<b<<endl;
+  
+  cout << "Address of a: " << &a << endl;
+  cout << "Address of b: " << &b << endl;
 
 //swap(a,b); //why does theirs work????   
  
-  myswap(a, b); // but mine doesn't ?!?!?!?!!!???
+  myswap(&a, &b); // but mine doesn't ?!?!?!?!!!???
 
   cout<<"a = "<<a<<", b = "<<b<<endl;
-
-
+  
+  cout << "Address of a: " << &a << " ";
+  cout << "Address of b: " << &b << endl;
 }
