d629677ec12fc8385988f7fb3387b5c1f8139dfd
Storm750 <Storm750@users.noreply.github.com>
2019-04-01 22:40:51 -0400

    It works! Deferenced ptr1 1st to pass the value, then def ptr2 to ptr1, and passed temp to def ptr2

--- src/pointers.cpp
+++ src/pointers.cpp
@@ -4,12 +4,12 @@ using namespace std;
 
 void myswap(int* ptr1, int* ptr2)
 {
-	
   cout << "ptr1 address: " << &ptr1 << endl;
+  cout << "address of ptr2: " << &ptr2 << endl << endl;
 
-  auto temp = ptr1;
-  ptr1 = ptr2;
-  ptr2 = temp;
+  auto temp = *ptr1;
+  *ptr1 = *ptr2;
+  *ptr2 = temp;
   
   cout << "address of temp: " << &temp << endl << endl;
   cout << "address of ptr1: " << &ptr1 << endl << endl;
@@ -19,7 +19,8 @@ void myswap(int* ptr1, int* ptr2)
 
 int main()
 {
-  int a = 25, b = 11;    
+  int a = 25, b = 11;  
+    
 
   cout<<"What am I doing wrong ☹ \n\n";
 
@@ -33,7 +34,4 @@ int main()
   myswap(&a, &b); // but mine doesn't ?!?!?!?!!!???
 
   cout<<"a = "<<a<<", b = "<<b<<endl;
-  
-  cout << "Address of a: " << &a << " ";
-  cout << "Address of b: " << &b << endl;
 }
